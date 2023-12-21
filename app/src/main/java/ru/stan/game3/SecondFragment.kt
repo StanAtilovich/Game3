e4b8f4cbc8d374f43ff298d2750888c04483e5b9
StanAtilovich <alano92@yandex.ru>
2023-12-21 17:45:27 +0800

    обновление игры и выбор игры во 2 фрагменте

--- app/src/main/java/ru/stan/game3/SecondFragment.kt
+++ app/src/main/java/ru/stan/game3/SecondFragment.kt
@@ -5,9 +5,14 @@ import android.content.ContentValues.TAG
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.RadioGroup
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
@@ -24,6 +29,7 @@ class SecondFragment : Fragment() {
     private lateinit var adapter: MemoryBoardAdapter
     private lateinit var memoryGame: MemoryGame
     private var boardSize: BoardSize = BoardSize.EASY
+    private lateinit var rvBoard: RecyclerView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,11 +41,15 @@ class SecondFragment : Fragment() {
             container,
             false
         )
+
+        rvBoard = view.findViewById(R.id.rvBoard)
+
         clRoot = view.findViewById(R.id.clRoot)
         tvNumPairs = view.findViewById(R.id.tvNumPairs)
         tvNumMoves = view.findViewById(R.id.tvNumMoves)
 
         memoryGame = MemoryGame(boardSize)
+
 
         adapter = MemoryBoardAdapter(
             requireContext(), boardSize, memoryGame.cards, object :
@@ -50,7 +60,7 @@ class SecondFragment : Fragment() {
             }
         )
 
-        val recyclerView: RecyclerView = view.findViewById(R.id.rvBoard)
+             val recyclerView: RecyclerView = view.findViewById(R.id.rvBoard)
         recyclerView.layoutManager = GridLayoutManager(requireContext(), boardSize.getWidth())
         recyclerView.adapter = adapter
 
@@ -89,4 +99,90 @@ class SecondFragment : Fragment() {
         tvNumMoves.text = "Moves: ${memoryGame.getNumMoves()}"
         adapter.notifyDataSetChanged()
     }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_main, menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.mi_refresh -> {
+                if (::memoryGame.isInitialized && memoryGame.getNumMoves() > 0 && !memoryGame.haveWonGame()) {
+                    showAlertDialog("Quit your current game?", null, View.OnClickListener { setUpBoard() })
+                } else {
+                    setUpBoard()
+                }
+                return true
+            }
+            R.id.mi_new_size -> {
+                showNewSizeDialog()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+
+
+
+    fun showNewSizeDialog() {
+        val boardSizeView =
+            LayoutInflater.from(requireContext()).inflate(R.layout.dialog_board_size, null)
+        val radioGroupSize = boardSizeView.findViewById<RadioGroup>(R.id.radioGroup)
+        when (boardSize) {
+            BoardSize.EASY -> radioGroupSize.check(R.id.rbEasy)
+            BoardSize.MEDIUM -> radioGroupSize.check(R.id.rbMedium)
+            BoardSize.HARD -> radioGroupSize.check(R.id.rbHard)
+        }
+        showAlertDialog("Choose new size", boardSizeView, View.OnClickListener {
+            boardSize = when (radioGroupSize.checkedRadioButtonId) {
+                R.id.rbEasy -> BoardSize.EASY
+                R.id.rbMedium -> BoardSize.MEDIUM
+                else -> BoardSize.HARD
+            }
+            setUpBoard()
+        })
+    }
+    private fun showAlertDialog(title: String, view: View?, positiveClickListener: View.OnClickListener) {
+        AlertDialog.Builder(requireContext())
+            .setTitle(title)
+            .setView(view)
+            .setNegativeButton("Cancel", null)
+            .setPositiveButton("OK") {_, _->
+                positiveClickListener.onClick(null)
+            }.show()
+    }
+    private fun setUpBoard() {
+        when (boardSize) {
+            BoardSize.EASY -> {
+                tvNumMoves.text = "Easy: 4 x 2"
+                tvNumPairs.text = "Pairs: 0 / 4"
+            }
+            BoardSize.MEDIUM -> {
+                tvNumMoves.text = "Medium: 6 x 3"
+                tvNumPairs.text = "Pairs: 0 / 9"
+            }
+            BoardSize.HARD -> {
+                tvNumMoves.text = "Hard: 6 x 4"
+                tvNumPairs.text = "Pairs: 0 / 12"
+            }
+        }
+        tvNumPairs.setTextColor(ContextCompat.getColor(requireContext(), R.color.color_progress_none))
+        memoryGame = MemoryGame(boardSize)
+
+        adapter = MemoryBoardAdapter(requireContext(), boardSize, memoryGame.cards, object: MemoryBoardAdapter.CardClickListener{
+            override fun onCardClicked(position: Int) {
+                updateGameWithFlip(position)
+            }
+
+        })
+        rvBoard.adapter = adapter
+        rvBoard.setHasFixedSize(true)
+        rvBoard.layoutManager = GridLayoutManager(requireContext(), boardSize.getWidth())
+    }
+
 }
