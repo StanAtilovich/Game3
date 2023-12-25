0d97798aff51a19561b1ed729b6ab9293d694be8
StanAtilovich <alano92@yandex.ru>
2023-12-25 08:25:35 +0800

    ротация homeFragment выполнена

--- app/src/main/java/ru/stan/game3/HomeFragment.kt
+++ app/src/main/java/ru/stan/game3/HomeFragment.kt
@@ -5,15 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.RadioButton
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.button.MaterialButton
 
 class HomeFragment : Fragment() {
     lateinit var rootView: View
     var oldTvScore2Value: Int = 0
+    var isEasyButtonChecked: Boolean = false
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,7 +24,7 @@ class HomeFragment : Fragment() {
     ): View? {
         rootView = inflater.inflate(R.layout.fragment_home, container, false)
 
-        rootView.findViewById<Button>(R.id.rbEasy).setOnClickListener {
+        rootView.findViewById<MaterialButton>(R.id.rbEasy).setOnClickListener {
             findNavController().navigate(R.id.action_homeFragment_to_secondFragment)
         }
 
@@ -36,9 +39,27 @@ class HomeFragment : Fragment() {
             }
         }
 
+        if (savedInstanceState != null) {
+            val savedScore = savedInstanceState.getInt("score")
+            rootView.findViewById<TextView>(R.id.tvScore2).text = "Score: $savedScore"
+            isEasyButtonChecked = savedInstanceState.getBoolean("isEasyButtonChecked")
+        }
+
+        rootView.findViewById<MaterialButton>(R.id.rbEasy).isChecked = isEasyButtonChecked
+
+        rootView.findViewById<MaterialButton>(R.id.action_homeFragment_to_secondFragment)
+
         return rootView
     }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val currentScore = rootView.findViewById<TextView>(R.id.tvScore2).text.toString().removePrefix("Score: ").toInt()
+        outState.putInt("score", currentScore)
+        outState.putBoolean("isEasyButtonChecked", rootView.findViewById<MaterialButton>(R.id.rbEasy).isChecked)
+    }
 }
+
 
 
 
