17500505bcb7bdbd41a54572b8249884f8b25028
StanAtilovich <alano92@yandex.ru>
2023-12-25 11:03:40 +0800

    Тестовое задание Junior Kotlin Developer Необходимо реализовать игру содержащий следующие экраны (Views) с соответствующей им логикой: 1. Menu View - главный навигационный экран, содержит кнопки перехода на другие экраны. 2. Game Scene - экран игры 3. End Game Popup - экран выйгрыша

--- app/src/main/java/ru/stan/game3/HomeFragment.kt
+++ app/src/main/java/ru/stan/game3/HomeFragment.kt
@@ -5,60 +5,44 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.RadioButton
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
-import com.google.android.material.button.MaterialButton
 
 class HomeFragment : Fragment() {
     lateinit var rootView: View
-    var oldTvScore2Value: Int = 0
-    var isEasyButtonChecked: Boolean = false
+    private var currentScore = 0 // Переменная для хранения текущего значения tvScore2
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         rootView = inflater.inflate(R.layout.fragment_home, container, false)
-
-        rootView.findViewById<MaterialButton>(R.id.rbEasy).setOnClickListener {
+        rootView.findViewById<Button>(R.id.rbEasy).setOnClickListener {
             findNavController().navigate(R.id.action_homeFragment_to_secondFragment)
         }
 
+        // Устанавливаем слушатель для получения результатов
         parentFragmentManager.setFragmentResultListener(
             "scoreResult", this
         ) { requestKey, result ->
             val newScore = result.getInt("score")
-            val tvScore2TextView = rootView.findViewById<TextView>(R.id.tvScore2)
-            val currentScore = tvScore2TextView.text.toString().removePrefix("Score: ").toInt()
+            // Обновляем tvScore2 в соответствии с условиями
             if (newScore > currentScore) {
-                tvScore2TextView.text = "Score: $newScore"
+                rootView.findViewById<TextView>(R.id.tvScore2).text = "Score: $newScore"
+                currentScore = newScore
+            } else if (newScore < currentScore) {
+                rootView.findViewById<TextView>(R.id.tvScore2).text = "Score: $currentScore"
+            } else {
+                rootView.findViewById<TextView>(R.id.tvScore2).text = "Score: $newScore"
             }
         }
 
-        if (savedInstanceState != null) {
-            val savedScore = savedInstanceState.getInt("score")
-            rootView.findViewById<TextView>(R.id.tvScore2).text = "Score: $savedScore"
-            isEasyButtonChecked = savedInstanceState.getBoolean("isEasyButtonChecked")
-        }
-
-        rootView.findViewById<MaterialButton>(R.id.rbEasy).isChecked = isEasyButtonChecked
-
-        rootView.findViewById<MaterialButton>(R.id.action_homeFragment_to_secondFragment)
-
         return rootView
     }
-
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        val currentScore = rootView.findViewById<TextView>(R.id.tvScore2).text.toString().removePrefix("Score: ").toInt()
-        outState.putInt("score", currentScore)
-        outState.putBoolean("isEasyButtonChecked", rootView.findViewById<MaterialButton>(R.id.rbEasy).isChecked)
-    }
 }
+
 
 
 
